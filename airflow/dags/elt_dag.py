8db73b6823dbead4cca988e2d0341af2a324551d
Luan Nguyen <tailuan30092000@gmail.com>
2024-02-11 23:27:36 -0800

    Adding airflow

---
+++ airflow/dags/elt_dag.py
@@ -0,0 +1,6 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from docker.types import Mount
+from airflow.operators.python_operators import PythonOperator
+from airflow.operators.python_operators import PythonOperator
+
